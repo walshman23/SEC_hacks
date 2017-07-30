ca5debd55823601bc5038166f191f522f1b44194
Walshman23 <walshman23@users.noreply.github.com>
2017-07-30 10:41:48 -0500

    Save results to temp file in anticipation of future processing

--- sec_10x_search.sh
+++ sec_10x_search.sh
@@ -5,15 +5,22 @@
 START=`date -v-1d +%m/%d/%Y`
 TODAY=`date +%m/%d/%Y`
 
-#curl -f -s "https://searchwww.sec.gov/EDGARFSClient/jsp/EDGAR_MainAccess.jsp?search_text=cyber%20malicious%20intrusion%20virus&sort=Date&formType=Form10K&isAdv=true&stemming=false&numResults=100&fromDate=$START&toDate=$TODAY&numResults=100&prt=true" | grep opennew | sed  's/^.*opennew(//g' | sed 's/);.*$//g'
+
+# Write stuff to temp file in anticipation of groveling thru it
+# in a second pass for some reason.
+
+TMPFILE="$(mktemp -t sec_10x_search)"
+
+trap "rm -f '$TMPFILE'" EXIT INT HUP TERM
 
 
 for formtype in 10k 10q; do
 	for keyword in ransomware petya wannacry; do
-		echo Forms $formtype mentioning $keyword from $START to $TODAY
-		curl -f -s "https://searchwww.sec.gov/EDGARFSClient/jsp/EDGAR_MainAccess.jsp?search_text=${keyword}&formType=Form${formtype}&isAdv=true&stemming=false&numResults=100&fromDate=$START&toDate=$TODAY&numResults=100&prt=true" | grep opennew | sed  's/^.*opennew(//g' | sed 's/);.*$//g'
+		echo Forms $formtype mentioning $keyword from $START to $TODAY >> "${TMPFILE}"
+		curl -f -s "https://searchwww.sec.gov/EDGARFSClient/jsp/EDGAR_MainAccess.jsp?search_text=${keyword}&formType=Form${formtype}&isAdv=true&stemming=false&numResults=100&fromDate=$START&toDate=$TODAY&numResults=100&prt=true" | grep opennew | sed  's/^.*opennew(//g' | sed 's/);.*$//g' | head -1 >> "${TMPFILE}"
 	done
 done
+cat "${TMPFILE}"
 
 
 
